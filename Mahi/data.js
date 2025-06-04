ec7ecbb34a564becb887258b46467a4295e319e0
banothmahi <162912462+banothmahi@users.noreply.github.com>
2025-06-04 06:34:46 +0000

    basic

---
+++ Mahi/data.js
@@ -0,0 +1,7 @@
+let score=true
+console.log(typeof score);
+console.log(typeof(score));
+let varIn=Number(score)
+console.log(typeof varIn);
+console.log( varIn);
+
---
+++ Mahi/variables.js
@@ -0,0 +1,8 @@
+const accountId=1443223
+let accountem="hi@google.com"
+var accountp="3456"
+accountcity="hyd"
+
+//accountId=2
+
+console.log(accountId);
