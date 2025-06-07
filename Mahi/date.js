027845e62daf04e24f2aa60844a7c5f2634ef040
banothmahi <162912462+banothmahi@users.noreply.github.com>
2025-06-07 11:16:01 +0000

    basics

---
+++ Mahi/date.js
@@ -0,0 +1,6 @@
+let myCreatedDate =new Date("07-06-2025")
+//console.log(myCreatedDate.toLocaleString());
+let myTimeStamp=Date.now()
+//console.log(myTimeStamp);
+console.log(myCreatedDate.getTime());
+
---
+++ Mahi/string.js
@@ -0,0 +1,11 @@
+//let value=3
+//let negv=-value
+//let str1="hello"
+//let str2="welcome"
+//let str3=str1+str2
+//console.log(str3);
+//console.log("1"+2);
+//console.log(1+"2");
+//console.log("1"+"2");
+//console.log(1+2+"2");
+
